44c997e6fd931548477cb0a41afed58914c33f17
borys <borys.mukovnin@gmail.com>
2025-05-23 12:31:03 +0200

    feat: Database structure updates.

--- database/database_manager.py
+++
@@ -1,4 +0,0 @@
-
-# TODO: DATABASE TEAM - This entire module will be replaced/augmented by the database team.
-# The methods defined here serve as an API contract.
-# The current implementation uses SQLite for basic standalone functionality.
--- database/user_management.py
+++
@@ -1,2 +0,0 @@
-# TODO: DATABASE TEAM - This module relies on DatabaseManager.
-# Ensure password hashing (LN5030) and user data storage (LD4200) are correctly implemented.
